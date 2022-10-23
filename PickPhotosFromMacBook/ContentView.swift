d5ef9e4b0b3fdfacc445a3ddb8c1216fec6d4234
Ramill <ramill_ibragimov@icloud.com>
2022-10-23 13:23:08 +0300

    init

--- PickPhotosFromMacBook/ContentView.swift
+++ PickPhotosFromMacBook/ContentView.swift
@@ -8,14 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var photos: [NSImage] = []
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            Label("", systemImage: "plus.square.fill.on.square.fill")
+                .font(.title)
+                .onTapGesture {
+                    selectPhotos()
+                }
+            ScrollView {
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 200))]) {
+                    ForEach(photos, id: \.self) { photo in
+                        if photo.isValid == true {
+                            Image(nsImage: photo)
+                                .resizable()
+                                .frame(width: 200, height: 200)
+                                .cornerRadius(12)
+                                .shadow(radius: 5)
+                        }
+                    }
+                }
+            }
         }
         .padding()
+    }
+    
+    func selectPhotos() {
+        let panel = NSOpenPanel()
+        panel.prompt = "Choose Picture"
+        panel.showsResizeIndicator = true
+        panel.showsHiddenFiles = false
+        panel.allowsMultipleSelection = true
+        panel.allowedContentTypes = [.image, .jpeg, .png]
+        
+        if panel.runModal() == .OK {
+            if let fileUrl = panel.url?.path {
+                let photo = NSImage(contentsOf: URL(fileURLWithPath: fileUrl))
+                self.photos.append(photo!)
+            }
+        }
     }
 }
 
