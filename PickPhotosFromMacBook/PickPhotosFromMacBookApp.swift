c58074da1c7ca1e2266d8d39ef9ae02934088915
Ramill <ramill_ibragimov@icloud.com>
2022-10-23 13:09:57 +0300

    Initial Commit

---
+++ PickPhotosFromMacBook/PickPhotosFromMacBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  PickPhotosFromMacBookApp.swift
+//  PickPhotosFromMacBook
+//
+//  Created by Ramill Ibragimov on 23.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct PickPhotosFromMacBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
